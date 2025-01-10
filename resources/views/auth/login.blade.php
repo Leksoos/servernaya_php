9c12f43072168ed7df380416878897073f51f422
Leksoos <pertsevlexoos@yandex.ru>
2025-01-10 15:30:26 +0300

    финал

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -11,6 +11,8 @@
 <form action="/auth/authenticate" method="POST">
   @csrf
 
+  <div></div>
+
   <div class="mb-3">
     <label for="email" class="form-label">Email address</label>
     <input type="email" class="form-control" id="email" name="email">
